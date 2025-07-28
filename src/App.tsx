c149517525dfd6ec996e92920aaac3144ce6b395
Abner Isaí Juárez Hernández <abner23_08@hotmail.com>
2025-07-28 11:47:06 -0600

    feat: Eliminar la base de la ruta en la configuración del enrutador y ajustes en vercel.json

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import Footer from './components/Footer';
 import ScrollToTop from './components/ScrollTop';
 function App() {
   return (
-    <Router basename='/wlantelecom'>
+    <Router >
       <ScrollToTop />
       <NavBar />
       <Routes>
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/wlantelecom/',
+  base:'./',
   server: {
     proxy: {
       '/api': 'http://localhost:5173'
